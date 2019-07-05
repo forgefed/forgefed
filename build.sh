93f2dec9f32a4999d2865491fccaafa52c166b12
fr33domlover <fr33domlover@riseup.net>
2019-07-06 01:29:56 +0300

    Specify date and commit ID in spec titles

--- build.sh
+++ build.sh
@@ -2,19 +2,45 @@
 
 inputs="forgefed forgefed-vocabulary"
 
+git_branch=`git rev-parse --abbrev-ref HEAD`
+
+git_commit_id=`git rev-parse HEAD`
+
+git_commit_id_short=`git rev-parse --short HEAD`
+
+now=`date --utc +%Y-%m-%d`
+
+dirty () {
+    git diff-index --quiet HEAD --
+}
+
 render () {
     dir="$1"
     file="$2"
+
     if [ "$3" == "true" ]; then
         toc="--table-of-contents"
     else
         toc=""
     fi
+
+    dirty
+    if [ $? -eq 0 ]; then
+        gitdirty=""
+    else
+        gitdirty="--variable gitdirty"
+    fi
+
     pandoc $dir/$file.md \
         --from markdown \
         --to html \
         --template html/template.html \
         $toc \
+        $gitdirty \
+        --variable "gitbranch:$git_branch" \
+        --variable "gitcommitid:$git_commit_id" \
+        --variable "gitcommitidshort:$git_commit_id_short" \
+        --variable "date:$now" \
         --number-sections \
         --output html/$file.html
 }
