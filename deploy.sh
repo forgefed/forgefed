6822769fd88db916bb2aa383a48d28ef24a49adc
6543 <6543@noreply.codeberg.org>
2022-06-17 15:53:37 +0200

    Deploy Website via CI (#117)
    Co-authored-by: 6543 <6543@obermui.de>
    Reviewed-on: https://codeberg.org/ForgeFed/ForgeFed/pulls/117
    Reviewed-by: Ta180m <ta180m@noreply.codeberg.org>

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-rsync \
-    --rsh='ssh -p 1234' \
-    --recursive \
-    --chown=hakyll:hakyll \
-    --compress \
-    --delete \
-    --verbose \
-    html/ \
-    hakyll@angeley.es:forgefed
+[ -z "${BOT_TOKEN}" ] && { echo "no token detected"; exit 1; }
+
+git clone https://${BOT_TOKEN}@codeberg.org/ForgeFed/pages.git /tmp/pages
+rm -fr /tmp/pages/*
+cp -rva html/* /tmp/pages
+cd /tmp/pages
+cat > .domains <<EOF
+forgefed.org
+www.forgefed.org
+EOF
+git add .
+if git diff --staged --exit-code >& /dev/null ; then
+    echo No changes to push
+else
+    git config user.email forgefedbot@forgefed.org
+    git config user.name 'ForgeFedBot'
+    git commit -m 'Update from deploy.sh'
+    git push -u origin main
+fi
